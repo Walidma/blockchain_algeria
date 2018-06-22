8fc778481fe9c8cab55e6425a828a4388c1be5da
WAWA <walid.mohand.amer@efrei.net>
2018-06-22 06:59:43 +0200

    A Working Decentralized Blockchain Network.

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,10 +1,15 @@
+const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3];
+
 function Blockchain() {
 	this.chain = [];
 	this.pendingTransactions = [];
 
+	this.currentNodeUrl = currentNodeUrl;
+	this.networkNodes = [];
+
 	this.createNewBlock(100, '0', '0');
 }
-const sha256 = require('sha256');
 
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
